bc24683d670baa4bd7145649bcd835c0cd84acd9
Zach <zhyun@cpp.edu>
2017-09-14 20:55:01 -0700

    Standard asset/control update
    Added 2D standard assets, along with basic platform assets. Basic
    controller for player and animator structure

---
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
+
+
+        private void Awake()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
+
+
+        private void Update()
+        {
+            if (!m_Jump)
+            {
+                // Read the jump input in Update so button presses aren't missed.
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            int attackType = 0;
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+                attackType = 3;
+            else if (Input.GetKeyDown(KeyCode.Q))
+                attackType = 1;
+            else if (Input.GetKeyDown(KeyCode.E))
+                attackType = 2;
+            // Pass all parameters to the character control script.
+            m_Character.Attack(attackType);
+            m_Character.Move(h, crouch, m_Jump);
+            m_Jump = false;
+        }
+    }
+}
---
+++ Assets/Standard Assets/2D/Scripts/RangedTurret.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedTurret : EnemyActor {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(IsEnemyInRange());
+	}
+
+
+
+    private bool IsEnemyInRange()
+    {
+        if(target != null && (target.transform.position - transform.position).magnitude <= rangeRadius)
+        {
+            return true;
+        }
+        return false;
+    }
+}
